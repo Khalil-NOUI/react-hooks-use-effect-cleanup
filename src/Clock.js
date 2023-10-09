536e496f01d6680593b2bdca54b05e047e524daf
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-10-09 03:51:22 +0100

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,9 +4,14 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const stopper = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+
+    return function cleanup() {
+      clearInterval(stopper)
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
